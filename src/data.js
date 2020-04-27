242c03b6cd64396acadcdbac12af3240e496603a
Tshepi <tshepi.jm@gmail.com>
2020-04-27 18:08:07 +0200

    storeapp

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Pine Paradise",
+    img: "img/product-1.png",
+    price: 10,
+    company: "",
+    info:
+      "Granadilla, Pine, Carrot flavoured Smoothies with homemade yoghurt",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "BanaBerry Beatz",
+    img: "img/product-2.png",
+    price: 16,
+    company: "",
+    info:
+      "Banana, Strawberry flavoured smoothies",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Berry Blaze",
+    img: "img/product-3.png",
+    price: 8,
+    company: "",
+    info:
+      "Strawberry and tint of berries smoothies",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Blueberry Heavenly huckleberry",
+    img: "img/product-4.png",
+    price: 18,
+    company: "",
+    info:
+      "BLueberry flavoured smoothie with a tint of oatmeal",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Banana Blender",
+    img: "img/product-5.png",
+    price: 24,
+    company: "",
+    info:
+      "Banana and Vanilla smoothies topped with a tint of cinnamon toping",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Kale",
+    img: "img/product-6.png",
+    price: 17,
+    company: "",
+    info:
+      "Kale flavoured smoothie with apple flavoured yoghurt",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Green Mamba",
+    img: "img/product-7.png",
+    price: 30,
+    company: "",
+    info:
+      "Spinash, Apples and lemon smoothies topped with mint smoothie",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Tropical Orange",
+    img: "img/product-8.png",
+    price: 2,
+    company: "",
+    info:
+      "Vitamin C infused from Lemon and Oranges smoothie",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Pine Paradise",
+  img: "img/product-1.png",
+  price: 10,
+  company: "",
+  info:
+    "Granadilla, Pine, Carrot flavoured Smoothies with homemade yoghurt",
+  inCart: false,
+  count: 0,
+  total: 0
+};
